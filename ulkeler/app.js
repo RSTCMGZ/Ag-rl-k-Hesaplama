1b6f5233e22d894571d3f5ad7e07b0efa8309af6
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-11-16 00:37:20 +0300

    finish-project

--- ulkeler/app.js
+++ ulkeler/app.js
@@ -6,11 +6,25 @@ wrap.style.gap = "20px";
 
 
 
-for (let i of countries) {
-    let p = document.createElement("p")
-    p.textContent = i
-    p.style.border = "1px solid black"
-    p.style.padding = "20px"
-    p.style.textAlign = "center"
-    wrap.append(p)
+function ekranaYazdır(x) {
+    for (let i of x) {
+        let p = document.createElement("p")
+        p.textContent = i
+        p.style.border = "1px solid black"
+        p.style.padding = "20px"
+        p.style.textAlign = "center"
+        wrap.append(p)
+    }
 }
+
+ekranaYazdır(countries)
+input.addEventListener("keyup", () => {
+    wrap.innerHTML = " ";
+    let dizi = []
+    for (let i of countries) {
+        if (i.toLowerCase().includes(input.value)) {
+            dizi.push(i)
+        }
+    }
+    ekranaYazdır(dizi)
+})
