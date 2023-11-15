636570b745daa4251b1bebff4368b76b4a49b253
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-11-15 23:53:04 +0300

    add-value's

--- gezegenler/app.js
+++ gezegenler/app.js
@@ -6,73 +6,80 @@ console.log(btn);
 const img = wrapper.querySelector("img")
 const p = wrapper.querySelector("p")
 
-// let gezegenler = [
-//     {
-//         'adi': 'dünya',
-//         'yercekimi': 10,
-//         'resim': './img/earth.png'
-//     },
-//     {
-//         'adi': 'jupiter',
-//         'yercekimi': 25,
-//         'resim': './img/jupiter.png'
-//     },
-//     {
-//         'adi': 'mars',
-//         'yercekimi': 3.7,
-//         'resim': './img/mars.png'
-//     },
-//     {
-//         'adi': 'mercur',
-//         'yercekimi': 3.7,
-//         'resim': './img/mercury.png'
-//     },
-//     {
-//         'adi': 'ay',
-//         'yercekimi': 1.6,
-//         'resim': './img/moon.png'
-//     },
-//     {
-//         'adi': 'neptün',
-//         'yercekimi': 11.1,
-//         'resim': './img/neptune.png'
-//     },
-//     {
-//         'adi': 'pluto',
-//         'yercekimi': 0.6,
-//         'resim': './img/pluto.png'
-//     },
-//     {
-//         'adi': 'saturn',
-//         'yercekimi': 10.4,
-//         'resim': './img/saturn.png'
-//     },
-//     {
-//         'adi': 'uranus',
-//         'yercekimi': 8.8,
-//         'resim': './img/uranus.png'
-//     },
-//     {
-//         'adi': 'venus',
-//         'yercekimi': 8.8,
-//         'resim': './img/venus.png'
-//     },
+let gezegenler = [
+    {
+        'adi': 'dünya',
+        'yercekimi': 10,
+        'resim': './img/earth.png'
+    },
+    {
+        'adi': 'jupiter',
+        'yercekimi': 25,
+        'resim': './img/jupiter.png'
+    },
+    {
+        'adi': 'mars',
+        'yercekimi': 3.7,
+        'resim': './img/mars.png'
+    },
+    {
+        'adi': 'mercur',
+        'yercekimi': 3.7,
+        'resim': './img/mercury.png'
+    },
+    {
+        'adi': 'ay',
+        'yercekimi': 1.6,
+        'resim': './img/moon.png'
+    },
+    {
+        'adi': 'neptün',
+        'yercekimi': 11.1,
+        'resim': './img/neptune.png'
+    },
+    {
+        'adi': 'pluto',
+        'yercekimi': 0.6,
+        'resim': './img/pluto.png'
+    },
+    {
+        'adi': 'saturn',
+        'yercekimi': 10.4,
+        'resim': './img/saturn.png'
+    },
+    {
+        'adi': 'uranus',
+        'yercekimi': 8.8,
+        'resim': './img/uranus.png'
+    },
+    {
+        'adi': 'venus',
+        'yercekimi': 8.8,
+        'resim': './img/venus.png'
+    },
 
-// ]
+]
 
-let gezegenler = ["dünya", "jüpiter", "mars", "merkür", "ay", "neptün", "pluton", "satürn", "uranüs", "venus"]
+// let gezegenler = ["dünya", "jüpiter", "mars", "merkür", "ay", "neptün", "pluton", "satürn", "uranüs", "venus"]
 
 for (let i of gezegenler) {
-    const option = document.createElement("option")
-    option.textContent = i[0].toUpperCase() + i.slice(1)
-    option.value = i
-    select.append(option)
+
+    // const option = document.createElement("option")
+    // option.textContent = i[0].toUpperCase() + i.slice(1)
+    // option.value = i
+    // select.append(option)
 }
 
 btn.addEventListener("click", () => {
 
     if (!isNaN(input.value) && (select.value != "Bir Gezegen Seçiniz")) {
         let value = Number(input.value)
-
+        let gezegen = select.value
+        let kutle = value / 10
+        if (gezegen == "dünya") {
+            img.setAttribute("src", "./img/earth.png")
+            img.className = "active"
+            p.textContent = `${gezegen} gezegenindeki ağırlığın ${kutle * 10} KG'DIR`
+        }
     }
 })
